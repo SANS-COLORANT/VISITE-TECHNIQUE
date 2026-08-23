4106189fbfe39bbd1b3feb13418c80f42e521721
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-08-23 13:26:40 +0200

    Register catalog provenance migration

--- database/migrations/index.js
+++ database/migrations/index.js
@@ -6,5 +6,6 @@ import { migration005 } from './005_persistent_networks_meters.js';
 import { migration006 } from './006_remark_links.js';
 import { migration007 } from './007_express_visits.js';
 import { migration008 } from './008_equipment_reference_details.js';
+import { migration009 } from './009_catalog_provenance_media.js';
 
-export const MIGRATIONS = Object.freeze([migration001, migration002, migration003, migration004, migration005, migration006, migration007, migration008]);
+export const MIGRATIONS = Object.freeze([migration001, migration002, migration003, migration004, migration005, migration006, migration007, migration008, migration009]);
