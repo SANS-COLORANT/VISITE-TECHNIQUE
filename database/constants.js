c0ba4d68556f93ee70a92c8053f7cff8d27481f9
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-08-23 13:26:48 +0200

    Bump database schema to version 9

--- database/constants.js
+++ database/constants.js
@@ -1,6 +1,6 @@
 export const DATABASE_NAME = 'visite_technique.db';
 
-export const DATABASE_SCHEMA_VERSION = 8;
+export const DATABASE_SCHEMA_VERSION = 9;
 
 export const ENTITY_TYPES = Object.freeze({
   CLIENT: 'client',
