ea2f67cb4bef9041658f8446d91333dfa33b4382
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-08-23 13:56:55 +0200

    Run second deep equipment enrichment seed

--- database/index.js
+++ database/index.js
@@ -6,6 +6,7 @@ import { seedEquipmentCatalog } from './equipmentCatalogSeed.js';
 import { seedEquipmentCatalogExtra } from './equipmentCatalogExtraSeed.js';
 import { seedEquipmentCatalogBreadth } from './equipmentCatalogBreadthSeed.js';
 import { seedEquipmentCatalogDeep } from './equipmentCatalogDeepSeed.js';
+import { seedEquipmentCatalogDeep2 } from './equipmentCatalogDeepSeed2.js';
 import { seedEquipmentCatalogImages } from './equipmentCatalogImageSeed.js';
 
 let databasePromise = null;
@@ -23,6 +24,7 @@ export function openAppDatabase() {
       await seedEquipmentCatalogExtra(db);
       await seedEquipmentCatalogBreadth(db);
       await seedEquipmentCatalogDeep(db);
+      await seedEquipmentCatalogDeep2(db);
       await seedEquipmentCatalogImages(db);
       return db;
     })().catch((error) => {
