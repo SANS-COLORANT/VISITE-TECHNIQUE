328457195bce944e74631e1bdb3924e149c01e75
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-08-24 07:44:41 +0200

    Passe le schéma SQLite en version 14

--- database/constants.js
+++ database/constants.js
@@ -1,6 +1,6 @@
 export const DATABASE_NAME = 'visite_technique.db';
 
-export const DATABASE_SCHEMA_VERSION = 13;
+export const DATABASE_SCHEMA_VERSION = 14;
 
 export const ENTITY_TYPES = Object.freeze({
   CLIENT: 'client',
