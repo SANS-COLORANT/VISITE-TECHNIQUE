61d117c421b9c4196c9ee353b502dffcd4df7607
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-08-25 14:08:07 +0200

    Register patrimoine history migration

--- database/migrations/index.js
+++ database/migrations/index.js
@@ -14,6 +14,7 @@ import { migration013 } from './013_material_excel_fields.js';
 import { migration014 } from './014_native_production_cleanup.js';
 import { migration015 } from './015_visit_runtime_indexes.js';
 import { migration016 } from './016_visit_template_id.js';
+import { migration017 } from './017_patrimoine_history.js';
 
 export const MIGRATIONS = Object.freeze([
   migration001,
@@ -32,4 +33,5 @@ export const MIGRATIONS = Object.freeze([
   migration014,
   migration015,
   migration016,
+  migration017,
 ]);
