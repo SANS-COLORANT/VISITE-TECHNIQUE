68a10361bf18adbcb7a1342281671a88b9d3033a
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-08-26 22:31:02 +0200

    Export reserve creation date in remarks column C

--- trameRegistry.js
+++ trameRegistry.js
@@ -136,7 +136,7 @@ const ICPE = Object.freeze({
         startRow: 4,
         maxImportRow: 500,
         columns: [['A', 'poste'], ['B', 'prestation'], ['D', 'delai'], ['F', 'estimatif']],
-        exportColumns: [['A', 'poste'], ['B', 'prestation'], ['D', 'delai'], ['F', 'estimatif']],
+        exportColumns: [['A', 'poste'], ['B', 'prestation'], ['C', 'date_reserve'], ['D', 'delai'], ['F', 'estimatif']],
       },
       note: { sheet: 'NOTE', cell: 'A2' },
     },
