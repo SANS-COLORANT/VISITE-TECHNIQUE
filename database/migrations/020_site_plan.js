5b3680f9de4bfd3ffd69bb954725fee5a09ac348
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-09-01 10:41:58 +0200

    Fix site plan migration format

--- database/migrations/020_site_plan.js
+++ database/migrations/020_site_plan.js
@@ -1,7 +1,7 @@
 export const migration020 = {
-  id: 20,
+  version: 20,
   name: 'site_plan',
-  async up(db) {
-    await db.execAsync(`ALTER TABLE sites ADD COLUMN plan_uri TEXT;`);
-  },
+  sql: `
+    ALTER TABLE sites ADD COLUMN plan_uri TEXT;
+  `,
 };
