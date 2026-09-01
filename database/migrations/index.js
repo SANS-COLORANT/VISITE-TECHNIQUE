4b57f8e799b6c8b6671796bccb3a8b68b10f9057
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-09-01 15:53:04 +0200

    feat: make pre-allumage visits modular

--- database/migrations/index.js
+++ database/migrations/index.js
@@ -18,10 +18,12 @@ import { migration017 } from './017_patrimoine_history.js';
 import { migration018 } from './018_remark_element_labels.js';
 import { migration019 } from './019_equipment_trame_usage.js';
 import { migration020 } from './020_site_plan.js';
+import { migration021 } from './021_pre_allumage_modular.js';
 
 export const MIGRATIONS = Object.freeze([
   migration001, migration002, migration003, migration004, migration005,
   migration006, migration007, migration008, migration009, migration010,
   migration011, migration012, migration013, migration014, migration015,
   migration016, migration017, migration018, migration019, migration020,
+  migration021,
 ]);
