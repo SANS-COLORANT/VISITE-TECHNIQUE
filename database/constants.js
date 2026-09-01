5963d39397756f99ecba4041e4b09a8f4e99c01a
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-09-01 15:53:00 +0200

    feat: make pre-allumage visits modular

--- database/constants.js
+++ database/constants.js
@@ -1,6 +1,6 @@
 export const DATABASE_NAME = 'visite_technique.db';
 
-export const DATABASE_SCHEMA_VERSION = 20;
+export const DATABASE_SCHEMA_VERSION = 21;
 
 export const ENTITY_TYPES = Object.freeze({
   CLIENT: 'client',
