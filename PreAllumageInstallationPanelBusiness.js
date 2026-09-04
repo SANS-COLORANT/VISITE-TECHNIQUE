e2158aa53f752bac824725c5e8234015ec903b7f
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-09-04 23:11:49 +0200

    Restore pre-allumage site structure on open

--- PreAllumageInstallationPanelBusiness.js
+++ PreAllumageInstallationPanelBusiness.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, View } from 'react-native';
 import { chargerPreAllumageModulaire } from './preAllumageModularDb.js';
 import { preparerStructurePreAllumage } from './preAllumageStructureDb.js';
+import { assurerStructureSitePreAllumage } from './preAllumageSiteBootstrap.js';
 import { PreAllumageInstallationPanelV3 } from './PreAllumageInstallationPanelV3.js';
 import { COLORS } from './styles.js';
 
@@ -11,6 +12,7 @@ export function PreAllumageInstallationPanelBusiness(props) {
     let alive = true;
     (async () => {
       await chargerPreAllumageModulaire(props.visiteId);
+      await assurerStructureSitePreAllumage(props.visiteId);
       await preparerStructurePreAllumage(props.visiteId);
       if (alive) setReady(true);
     })().catch((e) => {
