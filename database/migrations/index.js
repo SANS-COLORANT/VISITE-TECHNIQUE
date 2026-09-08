66dc6e0b8a5237e61a5ac35eefb531c62610b485
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-09-08 16:59:07 +0200

    feat(api): register client-site relation migration

--- database/migrations/index.js
+++ database/migrations/index.js
@@ -26,6 +26,7 @@ import { migration025 } from './025_pre_allumage_referentials.js';
 import { migration026 } from './026_pre_allumage_primary_usage.js';
 import { migration027 } from './027_symfony_api_cache.js';
 import { migration028 } from './028_symfony_preparation_integrity.js';
+import { migration029 } from './029_symfony_client_site_relations.js';
 
 export const MIGRATIONS = Object.freeze([
   migration001, migration002, migration003, migration004, migration005,
@@ -33,5 +34,5 @@ export const MIGRATIONS = Object.freeze([
   migration011, migration012, migration013, migration014, migration015,
   migration016, migration017, migration018, migration019, migration020,
   migration021, migration022, migration023, migration024, migration025,
-  migration026, migration027, migration028,
+  migration026, migration027, migration028, migration029,
 ]);
