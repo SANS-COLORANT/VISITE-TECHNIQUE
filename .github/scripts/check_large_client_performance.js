bda55578dc54ffecc0baad7cbf59daa6e054ffb8
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-09-11 15:01:14 +0200

    test: align client list virtualization contract

--- .github/scripts/check_large_client_performance.js
+++ .github/scripts/check_large_client_performance.js
@@ -27,7 +27,11 @@ requireText(home, 'const clientsPromise = listerClients()', 'home independent re
 
 const sites = read('ClientSitesScreen.js');
 requireText(sites, 'CLIENT_SITES_FAST_CACHE', 'client site cache');
-requireText(sites, 'removeClippedSubviews={false}', 'client site Android clipping guard');
+requireText(sites, 'initialNumToRender={14}', 'client site bounded first render');
+requireText(sites, 'maxToRenderPerBatch={10}', 'client site bounded render batches');
+requireText(sites, 'windowSize={6}', 'client site bounded render window');
+requireText(sites, 'removeClippedSubviews', 'client site Android virtualization');
+forbidText(sites, 'removeClippedSubviews={false}', 'client site clipping optimization disabled');
 
 const directoryScreen = read('MetraDirectoryScreen.js');
 requireText(directoryScreen, 'METRA_DIRECTORY_FAST_CACHE', 'Intranet directory render cache');
