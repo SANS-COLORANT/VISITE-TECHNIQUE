c22ece43252bbbfbc5a58efe763496e76c5ecfd1
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-09-11 13:51:51 +0200

    chore: register intranet photo outbox migration

--- database/migrations/index.js
+++ database/migrations/index.js
@@ -34,6 +34,7 @@ import { migration032 } from './032_photo_reference_workflow.js';
 import { migration033 } from './033_intranet_visit_outbox.js';
 import { migration034 } from './034_intranet_local_name_sync.js';
 import { migration035 } from './035_intranet_content_revision.js';
+import { migration036 } from './036_intranet_photo_outbox.js';
 
 export const MIGRATIONS = Object.freeze([
   migration001, migration002, migration003, migration004, migration005,
@@ -43,4 +44,5 @@ export const MIGRATIONS = Object.freeze([
   migration021, migration022, migration023, migration024, migration025,
   migration026, migration027, migration028, migration029, migration030,
   migration031, migration032, migration033, migration034, migration035,
+  migration036,
 ]);
