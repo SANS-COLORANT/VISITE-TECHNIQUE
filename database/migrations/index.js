6e184583c20e4821613d977f41deb2f4f4d15c57
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-09-14 18:53:07 +0200

    db: register Intranet SQL alignment migration

--- database/migrations/index.js
+++ database/migrations/index.js
@@ -34,6 +34,7 @@ import { migration033 } from './033_intranet_visit_outbox.js';
 import { migration034 } from './034_intranet_local_name_sync.js';
 import { migration035 } from './035_client_site_images.js';
 import { migration036 } from './036_intranet_visit_photo_outbox.js';
+import { migration037 } from './037_intranet_server_schema_alignment.js';
 
 export const MIGRATIONS = Object.freeze([
   migration001, migration002, migration003, migration004, migration005,
@@ -43,5 +44,5 @@ export const MIGRATIONS = Object.freeze([
   migration021, migration022, migration023, migration024, migration025,
   migration026, migration027, migration028, migration029, migration030,
   migration031, migration032, migration033, migration034, migration035,
-  migration036,
+  migration036, migration037,
 ]);
