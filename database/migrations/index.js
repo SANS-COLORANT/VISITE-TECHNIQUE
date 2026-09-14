d7533258674c89d19019d5f8994bbc5dc68e1eb5
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-09-14 15:29:57 +0200

    feat(patrimoine): enregistrer la migration des images

--- database/migrations/index.js
+++ database/migrations/index.js
@@ -29,10 +29,10 @@ import { migration028 } from './028_symfony_preparation_integrity.js';
 import { migration029 } from './029_symfony_client_site_relations.js';
 import { migration030 } from './030_large_client_performance_indexes.js';
 import { migration031 } from './031_latest_visit_photos.js';
-
 import { migration032 } from './032_photo_reference_workflow.js';
 import { migration033 } from './033_intranet_visit_outbox.js';
 import { migration034 } from './034_intranet_local_name_sync.js';
+import { migration035 } from './035_client_site_images.js';
 
 export const MIGRATIONS = Object.freeze([
   migration001, migration002, migration003, migration004, migration005,
@@ -41,5 +41,5 @@ export const MIGRATIONS = Object.freeze([
   migration016, migration017, migration018, migration019, migration020,
   migration021, migration022, migration023, migration024, migration025,
   migration026, migration027, migration028, migration029, migration030,
-  migration031, migration032, migration033, migration034,
+  migration031, migration032, migration033, migration034, migration035,
 ]);
