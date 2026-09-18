6c7f2cf8629b4a9f4dc5bb0d672ffa6b3811a5b9
SANS-COLORANT <154561309+SANS-COLORANT@users.noreply.github.com>
2026-09-18 10:25:51 +0200

    feat(missions): register v41 mission architecture migration

--- database/migrations/index.js
+++ database/migrations/index.js
@@ -38,6 +38,7 @@ import { migration037 } from './037_intranet_server_schema_alignment.js';
 import { migration038 } from './038_intranet_structure_creation.js';
 import { migration039 } from './039_intranet_structure_outbox_alignment.js';
 import { migration040 } from './040_missions_core.js';
+import { migration041 } from './041_missions_architecture.js';
 
 export const MIGRATIONS = Object.freeze([
   migration001, migration002, migration003, migration004, migration005,
@@ -48,4 +49,5 @@ export const MIGRATIONS = Object.freeze([
   migration026, migration027, migration028, migration029, migration030,
   migration031, migration032, migration033, migration034, migration035,
   migration036, migration037, migration038, migration039, migration040,
+  migration041,
 ]);
